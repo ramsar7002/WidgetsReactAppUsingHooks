fa72072331ec252dafb6727caf685cebe046aeed
Ram Sarfian <39989110+ramsar7002@users.noreply.github.com>
2021-04-06 08:05:05 +0300

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,68 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Accordion from './Accordion';
-import Search from './Search'
-import DropDown from './Dropdown'
-import Translate from './Translate'
+import Search from './Search';
+import DropDown from './Dropdown';
+import Translate from './Translate';
+import Route from './Route';
+import Header from './Header';
 
 const items = [
-    {
-        title: 'What is React?',
-        content: 'React is a front end javascript framework'
-    },
-    {
-        title: 'Why use React?',
-        content: 'React is a favorite JS among engineers'
-    },
-    {
-        title: 'How do you use React??',
-        content: 'You use react by creating component'
-    }
-]
-const options =[
-    {
-        label: 'The color Red',
-        value: 'red'
-    },
-    {
-        label: 'The color Green',
-        value: 'green'
-    },
-    {
-        label: 'A Shade of blue',
-        value: 'blue'
-    }
-]
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework',
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS among engineers',
+  },
+  {
+    title: 'How do you use React??',
+    content: 'You use react by creating component',
+  },
+];
+const options = [
+  {
+    label: 'The color Red',
+    value: 'red',
+  },
+  {
+    label: 'The color Green',
+    value: 'green',
+  },
+  {
+    label: 'A Shade of blue',
+    value: 'blue',
+  },
+];
 
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  return (
+    <div className="ui container">
+      <Header />
 
- const App = () =>{
-    const [selected, setSelected] = useState(options[0])
-    return(
-        <div className="ui container">
-            <Translate />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
 
-        </div>
-    )
-    
+      <Route path="/list">
+        <Search />
+      </Route>
+
+      <Route path="/dropdown">
+        <DropDown
+          label="select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+
+      <Route path="/translate">
+        <Translate />
+      </Route>
+    </div>
+  );
 };
 
 export default App;
--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,74 +1,77 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import Dropdown from './Dropdown';
-import axios from 'axios';;
+import axios from 'axios';
 
-const options =[
-    {
-        label: 'Hebrew',
-        value: 'he'
-    },
-    {
-        label: 'Spain',
-        value: 'es'
-    },
-    {
-        label: 'France',
-        value: 'fr'
-    }
-]
-const label = "Select a language:"
-const Translate = ()=>{
-    const [inputText, setInputText]= useState('');
-    const [selected, onSelectedChange] = useState(options[0]);
-    const [translated, setTranslated] = useState('');
+const options = [
+  {
+    label: 'Hebrew',
+    value: 'he',
+  },
+  {
+    label: 'Spain',
+    value: 'es',
+  },
+  {
+    label: 'France',
+    value: 'fr',
+  },
+  {
+    label: 'English',
+    value: 'en',
+  },
+];
+const label = 'Select a language:';
+const Translate = () => {
+  const [inputText, setInputText] = useState('');
+  const [selected, onSelectedChange] = useState(options[0]);
+  const [translated, setTranslated] = useState('');
 
-    const translateText = async()=>{
-        // console.log(inputText);
-        // console.log(selected.value);
-        const response =  await axios.post('https://translation.googleapis.com/language/translate/v2',{},{
-            
-           params:{ 
-            "q": `${inputText}`,
-            "target": `${selected.value}`,
-            "key": "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
-            }
-        })
-        console.log(response);
-        setTranslated(response.data.data.translations[0].translatedText)
+  const translateText = async () => {
+    // console.log(inputText);
+    // console.log(selected.value);
+    const response = await axios.post(
+      'https://translation.googleapis.com/language/translate/v2',
+      {},
+      {
+        params: {
+          q: `${inputText}`,
+          target: `${selected.value}`,
+          key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+        },
+      }
+    );
+    console.log(response);
+    setTranslated(response.data.data.translations[0].translatedText);
 
-        return(
-            <div dangerouslySetInnerHTML={{__html: translated}} />
-
-        )
-    }
-    return(
-        <div>
-            <label>Enter a text:</label>
-            <div className="ui fluid action input">
-            <input 
-            value ={inputText} 
-            type="text" 
-            placeholder="Search..."
-            onChange = {(e)=>setInputText(e.target.value)}
-            />
-            <div 
-            onClick={translateText}
-            className="ui button">Search
-            </div>
-            </div>
-
-            <Dropdown 
-            selected={selected}
-            onSelectedChange={onSelectedChange}
-            options={options}
-            label={label}
-            />
-            <div className="translation-area">
-                <label>Result</label>
-                <p>{translated}</p>
-            </div>
+    return <div dangerouslySetInnerHTML={{ __html: translated }} />;
+  };
+  return (
+    <div>
+      <label>Enter a text:</label>
+      <div className="ui fluid action input">
+        <input
+          value={inputText}
+          type="text"
+          placeholder="Search..."
+          onChange={e => setInputText(e.target.value)}
+        />
+        <div onClick={translateText} className="ui button">
+          Search
         </div>
-    )
-}
+      </div>
 
-export default Translate
+      <Dropdown
+        selected={selected}
+        onSelectedChange={onSelectedChange}
+        options={options}
+        label={label}
+      />
+      <div className="translation-area">
+        <label>Result</label>
+        <p>{translated}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Translate;
